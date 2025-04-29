cec2d78be9f530fe42dc62852639c6a7f5888be8
Woodaengtang <jhwoo200@gmail.com>
2025-04-29 17:18:48 +0900

    Mathematic idiot

--- lib/getFoci.m
+++ lib/getFoci.m
@@ -2,7 +2,7 @@ function [C1, C2] = getFoci(scale, rotation, intCPA, R_safe)
     
     a = R_safe*scale(1);
     b = R_safe*scale(2);
-    c = a^2 - b^2;
+    c = sqrt(a^2 - b^2);
     
     c1 = [c; 0; 0];
     c2 = [-c; 0; 0];
